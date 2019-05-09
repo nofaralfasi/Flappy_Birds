7df4ac35f671fe95da53cbd8a9b9e58b6ea9efe9
nofar <nofaralfasi@gmail.com>
2019-05-09 16:25:59 +0300

    updated mysql and birds' colors

--- flappybirds.sql
+++ flappybirds.sql
@@ -2,13 +2,13 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-CREATE DATABASE IF NOT EXISTS flappybirds;
-USE flappybirds;
+CREATE DATABASE IF NOT EXISTS 'flappybirds';
+USE 'flappybirds';
 
 
-CREATE TABLE IF NOT EXISTS highscores(
-  hs_id INT NOT NULL AUTO_INCREMENT,
-  hs_player VARCHAR(8) NOT NULL,
-  hs_score INT NOT NULL,
-  PRIMARY KEY (hs_id)
+CREATE TABLE IF NOT EXISTS 'highscores'(
+  'hs_id' INT NOT NULL AUTO_INCREMENT,
+  'hs_player' VARCHAR(8) NOT NULL,
+  'hs_score' INT NOT NULL,
+  PRIMARY KEY ('hs_id')
 ) ENGINE=InnoDB;
