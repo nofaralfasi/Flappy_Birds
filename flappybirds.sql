38a70b4eaa0a56cbdeac056593dd3d98d8a4f5ac
nofar <nofaralfasi@gmail.com>
2019-05-09 15:57:37 +0300

    updated mysql and birds' colors

--- flappybirds.sql
+++ flappybirds.sql
@@ -2,14 +2,13 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-CREATE DATABASE IF NOT EXISTS `flappybirds` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS flappybirds DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `flappybirds`;
 
 
-CREATE TABLE IF NOT EXISTS `highscores` (
-  `hs_id` int(11) NOT NULL AUTO_INCREMENT,
-  `hs_player` varchar(8) NOT NULL,
-  `hs_score` int(11) NOT NULL,
-  PRIMARY KEY (`hs_id`)
+CREATE TABLE IF NOT EXISTS highscores(
+  hs_id INT NOT NULL AUTO_INCREMENT,
+  hs_player VARCHAR(8) NOT NULL,
+  hs_score INT NOT NULL,
+  PRIMARY KEY (hs_id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
